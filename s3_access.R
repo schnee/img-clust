d00ce27c18ad7673fae9a1f74807be547c225e84
Brent Schneeman <schneeman@gmail.com>
2020-02-10 08:34:41 -0600

    starting to iteratively fetch 1k at a time

--- s3_access.R
+++ s3_access.R
@@ -19,9 +19,12 @@ b <- svc$list_buckets()
 my_bucket <- "app-secure-media"
 my_prefix <- "LilyAI/old_patterns_data/patterns"
 
-o <- svc$list_objects(Bucket=my_bucket, Prefix = my_prefix)
+o <- svc$list_objects_v2(Bucket=my_bucket, Prefix = my_prefix)
 
 images <- o$Contents %>% map("Key")
+
+o2 <- svc$list_objects_v2(Bucket=my_bucket, Prefix = my_prefix, StartAfter = images[length(images)])
+i2 <-images <- o2$Contents %>% map("Key")
 
 copy_image <- function(key, svc, the_bucket, the_prefix){
   
